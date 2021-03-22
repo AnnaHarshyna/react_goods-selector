37b5fbaa25e731afb2085caa51cef52d48104662
Anna <garsinaanna32778@gmail.com>
2021-03-22 19:30:31 +0200

    Updates

---
+++ precache-manifest.4baa843260d20e7a29377365cc14c803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93c5d87911f217296733dac951d036c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e03ae858951df3e20e53",
+    "url": "./static/css/main.ab71e97c.chunk.css"
+  },
+  {
+    "revision": "423d1f8f07466d4c3b37",
+    "url": "./static/js/2.0704ab71.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0704ab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e03ae858951df3e20e53",
+    "url": "./static/js/main.968078f8.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
